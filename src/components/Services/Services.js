282e10c67a9e97fc8367c65bff68cda3b676efb8
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-10-04 22:39:20 +0600

    services updated

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,40 +5,42 @@ import { ImOffice, ImBubble, ImBook } from "react-icons/im";
 
 const Services = () => {
     return (
-        <div className="sevices container my-5 py-5">
-            <div className="services mb-5">
-                <h1>We can help you with</h1>
-            </div>
-            <div className="mb-5">
-                <Row xs={1} md={3} className="g-4">
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title className="fs-1"><ImBook /></Card.Title>
-                                <Card.Title>Coaching</Card.Title>
-                                <Card.Text>Magnetized strongly enough pre vending domain overeus all initial results to estimate.</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title className="fs-1"><ImBubble /></Card.Title>
-                                <Card.Title>Consultation</Card.Title>
-                                <Card.Text>Magnetized strongly enough pre vending domain overeus all initial results to estimate.</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title className="fs-1"><ImOffice /></Card.Title>
-                                <Card.Title>Life Programs</Card.Title>
-                                <Card.Text>Magnetized strongly enough pre vending domain overeus all initial results to estimate.</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+        <div className="sevices ">
+            <div className="container">
+                <div className="services mb-5 text-light">
+                    <h1>We can help you with</h1>
+                </div>
+                <div className="mb-5">
+                    <Row xs={1} md={3} className="g-4">
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title className="fs-1"><ImBook /></Card.Title>
+                                    <Card.Title>Coaching</Card.Title>
+                                    <Card.Text className="px-5 py-3">Magnetized strongly enough pre vending domain overeus all initial results to estimate.</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title className="fs-1"><ImBubble /></Card.Title>
+                                    <Card.Title>Consultation</Card.Title>
+                                    <Card.Text className="px-5 py-3">Magnetized strongly enough pre vending domain overeus all initial results to estimate.</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title className="fs-1"><ImOffice /></Card.Title>
+                                    <Card.Title>Life Programs</Card.Title>
+                                    <Card.Text className="px-5 py-3">Magnetized strongly enough pre vending domain overeus all initial results to estimate.</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         </div>
 
